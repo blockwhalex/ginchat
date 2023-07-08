b3011664ccd234a72afab8d8f423668f07f6f538
simon <2942713658@qq.com>
2023-07-08 21:56:38 +0800

    服务器优雅重启

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -1,9 +1,16 @@
 package bootstrap
 
 import (
+	"context"
 	"ginchat/common"
 	"ginchat/route"
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func setupRouter() *gin.Engine {
@@ -19,8 +26,29 @@ func setupRouter() *gin.Engine {
 // RunServer 启动服务器
 func RunServer() {
 	r := setupRouter()
-	err := r.Run(":" + common.App.Config.App.Port)
-	if err != nil {
-		return
+
+	srv := &http.Server{
+		Addr:    ":" + common.App.Config.App.Port,
+		Handler: r,
 	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// 等待中断信号以优雅地关闭服务器（设置 5 秒的超时时间）
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
+
 }
